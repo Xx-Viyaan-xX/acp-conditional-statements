ad82cd87969592f981713545a1e841d00dd1b69b
Xx_Viyaan_xX <viyaantheking@gmail.com>
2024-10-14 15:14:29 +0530

    natural no.

---
+++ main.py
@@ -0,0 +1,13 @@
+number=int(input("enter a number to check if it is a natural no."))
+print("check the number",number)
+
+if number>0: 
+    print("it is a natural number")
+
+elif number<0: 
+    print("it is not a natural number number")
+
+else:
+    print("it is a neutral number")
+
+
